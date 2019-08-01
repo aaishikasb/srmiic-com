107637e1be0acad7a2eb5b68e3ede19302c2821c
9267aakashsharma <9267aakashsharma@gmail.com>
2019-08-01 15:51:09 +0530

    all startup page

--- res/js/navbar.js
+++ res/js/navbar.js
@@ -1,4 +1,4 @@
-$("#header__button").click(function() {
+$("#header__button").click(function () {
   $(this).toggleClass("hamburger--open");
   if ($(".kz-nav-menu").css("display") == "block") {
     $("body").removeClass("stop-scrolling");
@@ -7,14 +7,14 @@ $("#header__button").click(function() {
   } else {
     $("body").addClass("stop-scrolling");
     $(".kz-nav-menu").fadeIn(400);
-    $("body").bind("touchmove", function(e) {
+    $("body").bind("touchmove", function (e) {
       e.preventDefault();
     });
   }
 });
 
 function updateNavImage(index) {
-  const images = ["nav1.jpg", "nav2.jpg", "nav3.jpg", "nav4.jpg", "nav5.jpg"];
+  const images = ["nav1.jpg", "nav2.jpg", "nav3.jpg", "nav4.jpg", "nav5.jpg", "nav6.jpeg", "nav7.jpg"];
   var val =
     "linear-gradient(to bottom, rgba(255,255,255,0.4), rgba(255,255,255,0.8)),url(res/img/" +
     images[index] +
